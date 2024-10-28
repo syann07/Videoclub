df74b83fba58ce26a8fa95c887fd57785e9c1c45
Sonia <s.berabel@gmail.com>
2024-10-28 14:07:38 +0100

    Modificado

--- Soporte.php
+++ Soporte.php
@@ -1,3 +1,31 @@
 <?php
+declare (strict_types= 1);
 
+    class Soporte{
+
+        const IVA = 0.21;
+
+        function __construct(
+            public string $titulo, 
+            protected int $number,
+            private float $precio){}
+
+
+        public function getNumero(): int{
+            return $this->numero;
+        }
+
+        public function getPrecio(): float{
+            return $this->precio;
+        }
+        public function getPrecioConIVA(): float{
+            return $this->getPrecio() + $this->getPrecio()*self::IVA;
+        }
+
+        public function muestraResumen():void{
+
+        }
+
+       
+    }
 ?>
