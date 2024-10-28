c74fa68268446f2acc47e5b70bafb0607804e730
Daniel <danielvillalobosfdz@gmail.com>
2024-10-28 20:10:13 +0100

    Mostrar resumen

--- CintaVideo.php
+++ CintaVideo.php
@@ -1,5 +1,19 @@
 <?php
-    class CintaVideo {
-        private int $duracion;
+    class CintaVideo extends Soporte {
+        public function __construct(
+            private int $duracion,
+            string $titulo, 
+            int $number,
+            float $precio
+        ){
+            parent::__construct(
+                $titulo, $number, $precio
+            )
+        }
+
+        public function mostrarResumen(): void {
+            parent::mostrarResumen();
+            var_dump($duracion);
+        }
     }
 ?>
